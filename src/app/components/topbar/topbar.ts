282ec377ee3374bc939e1714d99964b3c96875d8
Kaue-C-Matos <kauecostamatos31@gmail.com>
2025-12-01 09:59:15 -0300

    Ajuste de tamanho e correção de bug

--- src/app/components/topbar/topbar.ts
+++ src/app/components/topbar/topbar.ts
@@ -1,8 +1,6 @@
-import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ActivatedRoute, RouterLink } from "@angular/router";
-import { userService } from '../../core/services/user';
-import { User } from '../../core/models/user';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-topbar',
@@ -11,21 +9,5 @@ import { User } from '../../core/models/user';
   styleUrl: './topbar.css',
 })
 export class Topbar {
-  private readonly route = inject(ActivatedRoute)
-  private readonly userId = Number(this.route.snapshot.paramMap.get("id"))
-  private readonly userService = inject(userService)
-  protected user: WritableSignal<User> = signal({} as User)
-  
-  ngOnInit(): void{
-    this.userService.getUser(this.userId!).subscribe((user)=>{
-      this.user.set({
-        id: user.id,
-        createdAt: user.createdAt,
-        profileName: user.profileName,
-        profilePhoto: user.profilePhoto
-      })
-    })
-  }
-
   loggedUser = 1
 }
